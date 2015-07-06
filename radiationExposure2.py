f485c52d4a65cd4be67ca177bfbcc34970bd7d2d
lgerrish <lauren.gerrish@gmail.com>
2015-07-05 22:22:08 -0400

    rewrote it

--- radiationExposure2.py
+++ radiationExposure2.py
@@ -8,20 +8,23 @@ def f(time):
 # DONE DELETING ME
 
 def radiationExposure(start, stop, step):
- area = 0
- current = start
- while current < stop:
-     length = f(current)
-     width = step
-     area += length * width
-     current+= step
- 
- return area 
+    sumOfAreas=0
+    current= start
+    while current< stop:
+        length= f(current)
+        width= step
+        area= length * width
+        current+= step
+        sumOfAreas+= area
+    
+    return sumOfAreas 
  
  
  
 # ALSO DELETEME
 # TEST CASES
+print "half, so 37.85ish"
+print radiationExposure(0,5,.5)
 print "39.103 ish?"
 print radiationExposure(0, 5, 1)
 print "22 ish?"
